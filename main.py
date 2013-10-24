3cae6227c78219ed9309c20b1a50f6244919c4e9
Charles Connell <charles@connells.org>
2013-10-24 16:47:07 -0400

    Command line args

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python
-import sys
-from WavInputFile import WavInputFile
 from error import *
 import wang
-from optparse import OptionParser
+from argparse import ArgumentParser
 
 def audio_matcher():
     """Our main control flow."""
 
-    parser = OptionParser()
-    parser.add_option("--test",
+    parser = ArgumentParser(description="Compare to audio files to determine if one"
+                                        "was derived from the other.")
+    parser.add_argument("-f", action="append", required=False, dest="files", default=list(),
+                        help="A file to examine. Must provide two instance of this argument.")
+    parser.add_argument("--test",
                   action="store_true", dest="test", default=False,
                   help="Run in test mode (shows stacktraces on error)")
 
-    (options, args) = parser.parse_args()
+    try:
+        args = parser.parse_args()
+    except Exception, e:
+        if args.test:
+            raise
+        die(e.message)
 
-    if len(sys.argv) < 3:
+    if len(args.files) < 2:
         die("Must provide two input files.")
 
     # Use our matching system
     try:
-        matcher = wang.Wang(sys.argv[1], sys.argv[2])
+        matcher = wang.Wang(args.files)
         if matcher.match():
             print "MATCH"
         else:
             print "NO MATCH"
     except IOError, e:
-        if options.test:
+        if args.test:
             raise
         die(e.message)
         return
